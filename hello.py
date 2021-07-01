dcccdba9cb13b336fc53b4b3b21e852f35c805b9
Darlhem J <joseph.darlhem@gmail.com>
2021-07-01 15:00:31 -0500

    first commit

---
+++ hello.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 28 11:10:28 2021
+
+@author: darlhemjoseph
+"""
+
+#This program will ask for the user name.
+
+def hello():
+    print("Hello World")
+    print("What is your name?")
+    name = input()
+    print("Its nice to meet you, " + name)
+    
+    
+
+
+hello()
