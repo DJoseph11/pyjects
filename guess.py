0f3a7f3bf09caabccad805c4539970c02924bf0e
Darlhem J <joseph.darlhem@gmail.com>
2021-07-07 06:31:13 -0500

    Added comments to some of the line fo code

--- guess.py
+++ guess.py
@@ -1,21 +1,22 @@
 #This is a Guess the number game program.
 import random
 
-guesseTaken = 0
+guesseTaken = 0 # This line of code sets a counter for the amount of guesses
 
 print("Hello! What is your name?")
-name = input()
+name = input() # Takes the name of the player
 
-number = random.randint(1, 100)
+number = random.randint(1, 100) # This line of code generate a number that is assigned to the number variable name
 print("Well, " + name + ", I am thinking of a number between 1 and 100.")
 
 
-for guesseTaken in range(6):
+for guesseTaken in range(6): # This line of code use a for loop to start guesseTaken at 0 up to 6 guesses
     print("Take a guess.")
-    guess = input()
-    guess = int(guess)
+    guess = input()  #Input is always entered as a string
+    guess = int(guess) 
 
-    if guess < number:
+# The conditions below consist of the main core of the game.
+    if guess < number: 
         print("Your guess is to low.")
 
     if guess > number:
@@ -33,5 +34,3 @@ if guess != number:
     print("Nope. The number I was thinking of was " + number + ".")
 
 
-# for i in range(3):
-#     print("hello! i is set to",i)
