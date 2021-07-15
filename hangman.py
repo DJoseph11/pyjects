13bdffb4582fe6e49e4c4b1002a34c26f3ab6084
Darlhem J <joseph.darlhem@gmail.com>
2021-07-14 20:33:48 -0500

    added two new function to code

--- hangman.py
+++ hangman.py
@@ -51,7 +51,43 @@ def get_random_word(word_list): # def example(any_name):
     return word_list[word_index] # returns get_random_word(list[index])
 # print(get_random_word(words))
 
+def display_board(missed_letters, correct_letters,secret_word):
+    # display_board function takes in three parameters: missed_letters, correct_letters, secret_word
+
+    print(HANGMAN_PICS[len(missed_letters)])
+    # prints out the hangman pic from the global variables HANGMAN_PICS list.
+    print()
+
+    print("Missed letters:", end=" ")
+    for letter in missed_letters:
+        print(letter, end=" ")
+
+    print()
+
+    blanks = "_" * len(secret_word)
+    # blanks = "_" * len(thundering) would result in 10 underscores __________ 
+
+    for i in range(len(secret_word)):
+        # exp len(thundering) = 10 ---> for i in range(10):
+        if secret_word[i] in correct_letters:
+            # if the letter at index i from the word is within the correct letters list
+            blanks = blanks[:i] + secret_word[i] + blanks[i + 1]
+            # blanks get updated to
+
+    for letter in blanks:
+        print(letter, end=" ")
+
+    print()
 
 
-
-
+def get_guess(already_guessed):
+    while True:
+        print("Guess a letter.")
+        guess = input()
+        guess = guess.toLower()
+        if len(guess) != 1:
+            print("Please enter a single letter.")
+        elif guess in already_guessed:
+            print("You have already guessed that letter. choose again.")
+        elif guess not in "abcdefghijklmnopqrstwxyz":
+            print("Please enter a LETTE")
